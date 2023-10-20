c1728d1fbb980982a8bde7f59a6d8a215b57e0f3
kenzatazilabzour <kenzatazi17@gmail.com>
2023-10-20 14:57:41 -0700

    second commit

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -18,9 +18,13 @@ export default function App() {
             <NavBar user={user} setUser={setUser} />
             <Routes>
               {/* Route components in here */}
+              <Route path="/" element={<HomePage />} />
+              <Route path="/auth" element={<AuthPage />} />
+              <Route path="/posts" element={<PostsPage />} />
               <Route path="/orders/new" element={<NewOrderPage />} />
               <Route path="/orders" element={<OrderHistoryPage />} />
               <Route path="/notes" element={<NoteListPage />} />
+            <Route path="/*" element={<Navigate to="/" />} />
             </Routes>
           </>
           :
